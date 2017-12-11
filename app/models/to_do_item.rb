abe1048b14920908875c5026e8658597ccae8f6a
kaiks <karol.lewcio@gmail.com>
2017-12-11 23:08:28 +0100

    start work on deadlines

--- app/models/to_do_item.rb
+++ app/models/to_do_item.rb
@@ -1,10 +1,22 @@
 class ToDoItem < ApplicationRecord
   belongs_to :user
   before_save :set_done_at, if: :done_changed?
+  before_create :set_valid_from
+  validate :dates_valid?
 
+  #RED = RGB::Color.from_rgb(255, 0, 0)
+  #YELLOW = RGB::Color.from_rgb(255, 255, 0)
 
   protected
   def set_done_at
     self.done_at = done? ? DateTime.now : nil
+  end
+
+  def set_valid_from
+    self.valid_from ||= DateTime.now
+  end
+
+  def dates_valid?
+    (self.deadline.nil? || self.valid_from < self.deadline) && self.deadline > DateTime.now
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171202233033) do
+ActiveRecord::Schema.define(version: 20171210184650) do
 
   create_table "list_items", force: :cascade do |t|
     t.text "url"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20171202233033) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "valid_from"
+    t.datetime "deadline"
   end
 
   create_table "users", force: :cascade do |t|
