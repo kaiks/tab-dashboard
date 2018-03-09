7a7543879daf6a2c90c6ee1fec971bd9b6d31a66
kaiks <karol.lewcio@gmail.com>
2018-03-09 23:36:50 +0100

    autofix urls for knowledge repositories

--- app/models/knowledge_repository.rb
+++ app/models/knowledge_repository.rb
@@ -1,2 +1,9 @@
 class KnowledgeRepository < ApplicationRecord
+  protected
+
+  def fix_url
+    unless self.url[/\Ahttp:\/\//] || self.url[/\Ahttps:\/\//]
+      self.url = "http://#{self.url}"
+    end
+  end
 end
